46bcd40eb9f44006928f1ad13af36ea796e1194d
Pankaj Pawar <Pankaj14321@protonmail.com>
2024-12-10 07:39:09 +0000

    port to pages and ignore env file

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import Aside from "@/components/Aside"; 
+import Form from "/components/Form";
+import './index.css';
+
+function Home() {
+ return(
+  <div>
+    <Header />
+    <Aside /> 
+    <Form />
+  </div>
+ )
+}
+
+export default Home;
